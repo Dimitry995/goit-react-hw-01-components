888d0db46346436c10f666ce98548dc173ed45db
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-01-26 13:23:55 +0200

    homework

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import { Profile } from './Profile/Profile';
+import { Statistics } from './Statistics/Statistics';
+import { Friends } from './Friends/Friends';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+export const App = () => {
+  return (
+    <>
+      <Profile />
+      <Statistics />
+      <Friends />
+      <TransactionHistory />
+    </>
+  );
+};
