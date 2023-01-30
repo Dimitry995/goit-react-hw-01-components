a89bf0f98470884395fc6651f5b23b53b838c311
Dimitry995 <98602983+Dimitry995@users.noreply.github.com>
2023-01-30 14:49:54 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,12 +1,16 @@
 import upcomingData from '../../components/stats.json';
 import { StatisticsWrapper, Title } from './Statistics.styled';
 import { StatisticsList } from './StatisticsList/StatisticsList';
-
-export const Statistics = () => {
-  return (
-    <StatisticsWrapper>
-      <Title>UPLOAD STATS</Title>
-      <StatisticsList data={upcomingData} />
-    </StatisticsWrapper>
-  );
+import PropTypes from 'prop-types'
+export const Statistics = ({title}) => {
+    return (
+        <StatisticsWrapper>
+            {title && <Title>{title}</Title>}
+            <StatisticsList data={upcomingData} />
+        </StatisticsWrapper>
+    );
+};
+ Statistics.propTypes = {
+    title: PropTypes.string,
+  stats: PropTypes.array.isRequired,
 };
